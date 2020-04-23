f1564926d81156edefd4410d6bb1117698c53051
Vishal Vaghela <vishalsbaghela@gmail.com>
2020-04-23 17:34:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 
-if($_SERVER['HTTP_HOST']=='www.a1gana.com'){
-	header('Location: http://a1gana.com'.$_SERVER['REQUEST_URI'],TRUE,301);
+if($_SERVER['HTTP_HOST']=='www.hifidj.in'){
+	header('Location: https://hifidj.in'.$_SERVER['REQUEST_URI'],TRUE,301);
 	exit;
 }
-define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/sumant'));
+define('SF_ROOT_DIR',    realpath(dirname(__FILE__).'/vishal_baghel'));
 define('SF_APP',         'frontend_m');
-define('SF_ENVIRONMENT', 'a1gana');
+define('SF_ENVIRONMENT', 'hifidj_env');
 define('SF_DEBUG',       false);
 
 require_once(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'apps'.DIRECTORY_SEPARATOR.SF_APP.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
